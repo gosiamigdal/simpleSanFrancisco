80a3458956ec0117034a8e39085f26cd6f4c1a5a
Malgorzata Migdal <malgorzata@migdal.pl>
2013-11-06 21:49:38 -0800

    Buttons changes color when clicked. JQuery involved

--- static/js/application.js
+++ static/js/application.js
@@ -1,3 +1,22 @@
 $(function() {
     $(".alert").alert();
+
+    $(".plan-buttons .btn").click( function (event) {
+        var cssClass = $(this).attr("class");
+        cssClass = cssClass.split("button-")[1];
+        //alert("Button clicked!" + cssClass); // TODO
+
+        $(".plan-buttons .btn").removeClass("toggled");
+        $(this).addClass("toggled");
+        
+        $(".plan-categories").hide();
+        $(this).parent().parent().children(".plan-categories").show();
+
+    });
+
+
+    // ...
+    // val cssClass = $(".plan-buttons .toggled").attr("class");
+    // cssClass = cssClass.split("button-")[1];
+    // /select/2pm/dining
 });
